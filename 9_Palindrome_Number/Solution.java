3d466d2fe7439f39f69e26a33384b445475141b6
Sukhdip <=>
2020-04-09 18:23:58 -0700

    solution - can be improved my only reversing half of the integer

---
+++ 9_Palindrome_Number/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int palindrome = x;
+        int reverse = 0;
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+        return x == reverse;
+    }
+}
